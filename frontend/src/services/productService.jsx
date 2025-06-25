3504f8431a18d2b42d3807735049640b9a345c4c
Vignesh Murugan <vigmurug@gmail.com>
2025-06-25 23:47:23 +0530

    feat: created the api service for products

--- frontend/src/services/productService.jsx
+++ frontend/src/services/productService.jsx
@@ -1,41 +1,41 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL|| 'http://localhost:5000/api';
 
 // Get all products
 export const getProducts = async () => {
-    const response = await axios.get(`${API_URL}/products`);
+    const response = await axios.get(`${API_URL}/product`);
     return response.data.data.products;
 };
 
 // Get product by ID
 export const getProductById = async (id) => {
-    const response = await axios.get(`${API_URL}/products/${id}`);
+    const response = await axios.get(`${API_URL}/product/${id}`);
     return response.data.data.product;
 };
 
 // Create a new product
 export const createProduct = async (productData) => {
-    const response = await axios.post(`${API_URL}/products`, productData);
+    const response = await axios.post(`${API_URL}/product`, productData);
     return response.data.data.product;
 }
 
 // Update a product
 export const updateProduct = async (id, productData) => {
-    const response = await axios.put(`${API_URL}/products/${id}`, productData);
+    const response = await axios.patch(`${API_URL}/product/${id}`, productData);
     return response.data.data.product;
 }
 
 // Delete a product
 export const deleteProduct = async (id) => {
-    const response = await axios.delete(`${API_URL}/products/${id}`);
+    const response = await axios.delete(`${API_URL}/product/${id}`);
     return response.data.data;
 }
 
 // Seed products from dummyJSON API
 export const seedProducts = async (clear) => {
     const url = clear
-        ? `${API_URL}/see/products?clear=true`
+        ? `${API_URL}/seed/products?clear=true`
         : `${API_URL}/seed/products`;
 
     const response = await axios.get(url);
